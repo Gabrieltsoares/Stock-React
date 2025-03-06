885cc43e879adc24964651e2469ab50e58c3d3cf
Gabrieltsoares <gabrdev1@outlook.com>
2025-03-05 21:07:50 -0300

    Estrutura inicial do projeto de Stock feito em react

---
+++ src/Contexts/StockContext.jsx
@@ -0,0 +1,9 @@
+import { createContext } from "react";
+import Proptypes from "prop-types"
+
+export const StockContext = createContext({})
+
+
+export function StockContextProvider ({children}) {
+
+}
